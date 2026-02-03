2b832d2c9314dc928ef355c242929192d82dc378
arivasv-cr <>
2026-02-03 18:03:18 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,14 @@
+start_number = 5
+countdown_values = []
+
+# List to store countdown values
+countdown_values = []
+
+# While loop countdown
+while start_number >= 1:
+    countdown_values.append(start_number)
+    start_number -= 1
+
+# Final output
+print("Discount countdown complete!")
+print(countdown_values)
